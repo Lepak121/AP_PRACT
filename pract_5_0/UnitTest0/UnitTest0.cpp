0e8ec001116f894c73b7c75c871fe9ee249fdd53
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-04 22:51:10 +0200

    Add files via upload

---
+++ pract_5_0/UnitTest0/UnitTest0.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pract_5_0/pract_5_0.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest0
+{
+	TEST_CLASS(UnitTest0)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = sum(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
