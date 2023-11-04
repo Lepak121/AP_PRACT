12247dc935fcbd9b4cdbba2e8e5a64a9aae59505
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-04 22:23:59 +0200

    Add files via upload

---
+++ Pract_5_1/Pract_5_1/Pract_5_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double g(const double a, const double b, const double c);
+
+int main() {
+    double x, y;
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+
+    double d = (g(x * y, x * x, y * y) - g(1, x, y) * g(1, x, y)) / (1 + g(sqrt(x), y * y, 1));
+
+    cout << "d = " << d << endl;
+
+    return 0;
+}
+
+double g(const double a, const double b, const double c) {
+    return (a * a + b * b - c * c);
+}
---
+++ Pract_5_1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_5_1/Pract_5_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double a = 2;
+			double b = 4;
+			double c = 1;
+			double result = (a * a + b * b - c * c);
+			double one = g(a, b, c);
+			Assert::AreEqual(result, one);
+		}
+	};
+}
+
