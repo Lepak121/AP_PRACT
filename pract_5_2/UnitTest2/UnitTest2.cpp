a482ca8ef99f889c4ee338bd58cee39226fb46e4
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-05 23:49:29 +0200

    Add files via upload

---
+++ pract_5_2/UnitTest2/UnitTest2.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pract_5_2/pract_5_2.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double x = 1;
+			double eps = 0.0000001;
+			int n = 0;
+			double s = 0;
+			double expected = atan(x);
+
+			double actual = S(x, eps, n, s);
+
+			Assert::AreEqual(expected, actual, eps);
+		}
+	};
+}
