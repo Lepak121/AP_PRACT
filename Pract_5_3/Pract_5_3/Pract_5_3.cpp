7f3377bac1fb50026474e3d7b2fe792be2ec2d22
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-06 01:34:51 +0200

    Add files via upload

---
+++ Pract_5_3/Pract_5_3/Pract_5_3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double s(const double x);
+
+int main()
+{
+    double tp, tk, z;
+    int n;
+    cout << "tp = "; cin >> tp;
+    cout << "tk = "; cin >> tk;
+    cout << "n = "; cin >> n;
+
+    double tg = (tk - tp) / n;
+    double t = tp;
+
+    while (t <= tk)
+    {
+        z = s(2 * t + 1) + 2 * s(t * t)+sqrt(s(1));
+        cout << t << " " << z << endl;
+        t += tg;
+    }
+
+    return 0;
+}
+
+double s(const double x)
+{
+    if (x != 0 && abs(x) >= 1)
+    {
+        return (pow(cos(x), 2) + 1) / exp(x) * exp(x);
+    }
+    else
+    {
+        double S = 0;
+        int k = 0;
+        double a = 1;
+        S = a;
+
+        do
+        {
+            k++;
+            double R = (pow(2, 2 * k + 1) * pow(x, 2 * k + 1)) / ((2 * k) * (3 * k + 1) * 2 * k);
+            a *= R;
+            S += a;
+        } while (k < 4);
+
+        return S;
+    }
+}
---
+++ Pract_5_3/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_5_3/Pract_5_3.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double x = 2.0;
+			double expected = (pow(cos(x), 2) + 1) / exp(x) * exp(x);
+
+			double actual = s(x);
+
+
+			Assert::AreEqual(expected, actual, 0.001);
+		}
+	};
+}
