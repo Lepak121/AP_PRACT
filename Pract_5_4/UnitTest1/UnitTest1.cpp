b7a07edc96a95ee8f99a68bdae47b396da42a41c
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-13 20:33:23 +0200

    Add files via upload

---
+++ Pract_5_4/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pract_5_4/pract_5_4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+          
+            int K = 4;
+            int N = 6;
+            double expected = 0.0;
+            for (int i = K; i <= N; i++) {
+                expected += pow(i, 2) / (pow(N, 2) + pow(K, 2));
+            }
+
+       
+            Assert::AreEqual(S0(K, N), expected);
+
+            int i = K;
+            Assert::AreEqual(S1(K, N, i), expected);
+
+            i = N;
+            Assert::AreEqual(S2(K, N, i), expected);
+
+            i = K;
+            double t = 0.0;
+            Assert::AreEqual(S3(K, N, i, t), expected);
+
+            i = N;
+            t = 0.0;
+            Assert::AreEqual(S4(K, N, i, t), expected);
+        }
+    };
+}
