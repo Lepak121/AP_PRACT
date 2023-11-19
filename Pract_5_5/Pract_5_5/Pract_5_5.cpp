11f54458602d555ba777de16f590303220fd965e
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-19 15:47:38 +0200

    Add files via upload

---
+++ Pract_5_5/Pract_5_5/Pract_5_5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+int c(int n, int k) {
+    if (k > n ) {
+        return 0;
+    }
+    else if (k == 0 || k == n) {
+        return 1;
+    }
+    else {
+        return c(n - 1, k - 1) +c(n - 1, k);
+    }
+}
+
+int D(int n, int k) {
+    if (k > n) {
+        return 0;
+    }
+    else if (k == 0 || k == n) {
+        return 1;
+    }
+    else {
+        return 1 + D(n - 1, k - 1) + D(n - 1, k);
+    }
+}
+
+int main() {
+    int n, k;
+    cout << "n= ";
+    cin >> n;
+    cout << "k= ";
+    cin >> k;
+
+    int C = c(n, k);
+    cout << "C(" << n << ", " << k << ") = " << C << std::endl;
+    cout << "Glybyna rekursiyi = " << D(n, k) << std::endl;
+
+    return 0;
+}
---
+++ Pract_5_5/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_5_5/Pract_5_5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(BinomTest)
+    {
+    public:
+
+        TEST_METHOD(PositiveBinomTest)
+        {
+            const int n = 5;
+            const int k = 3;
+
+            Assert::AreEqual(binom(n, k), 10);
+        }
+
+        TEST_METHOD(NegativeBinomTest)
+        {
+            const int n = 5;
+            const int k = 6;
+
+            Assert::AreEqual(binom(n, k), 0);
+        }
+      
+
+        TEST_METHOD(NegativeBinomDepthTest)
+        {
+            const int n = 5;
+            const int k = 6;
+
+            Assert::AreEqual(binom_depth(n, k), 0);
+        }
+    };
+}
