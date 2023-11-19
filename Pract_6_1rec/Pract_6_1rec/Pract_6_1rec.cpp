31420f25ba844844f3bd326952385b65c807f279
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-19 19:09:23 +0200

    Add files via upload

---
+++ Pract_6_1rec/Pract_6_1rec/Pract_6_1rec.cpp
@@ -0,0 +1,52 @@
+﻿#include <time.h>
+#include <iostream>
+using namespace std;
+
+void applyCriteria(int array[], int index, int arraySize, int& count, int& sum) {
+    if (index < arraySize) {
+        if (array[index] > 0 && array[index] % 3 != 0) {
+            sum += array[index];
+            count++;
+            array[index] = 0;
+        }
+        applyCriteria(array, index + 1, arraySize, count, sum);
+    }
+}
+
+void printArray(const int array[], int index, int arraySize) {
+    if (index < arraySize) {
+        cout << array[index] << " ";
+        printArray(array, index + 1, arraySize);
+    }
+    else {
+        cout << "\n";
+    }
+}
+
+void initializeArray(int array[], int index, int arraySize) {
+    if (index < arraySize) {
+        array[index] = -10 + rand() % (35 + 10 + 1);
+        initializeArray(array, index + 1, arraySize);
+    }
+}
+
+int main()
+{
+    const int arraySize = 22;
+    int sum = 0;
+    int count = 0;
+    int array[arraySize];
+    srand((unsigned)time(NULL));
+
+    initializeArray(array, 0, arraySize);
+    printArray(array, 0, arraySize);
+
+    applyCriteria(array, 0, arraySize, count, sum);
+
+    cout << "sum: " << sum << "\n";
+    cout << "count: " << count << "\n";
+
+    printArray(array, 0, arraySize);
+
+    return 0;
+}
---
+++ Pract_6_1rec/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_6_1rec/Pract_6_1rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestInitializeArray)
+        {
+            const int arraySize = 22;
+            int array[arraySize];
+            initializeArray(array, 0, arraySize);
+
+            for (int i = 0; i < arraySize; i++) {
+                Assert::IsTrue(array[i] >= -10 && array[i] <= (35 + 10));
+            }
+        }
+
+        TEST_METHOD(TestApplyCriteria)
+        {
+            const int size = 22;
+            int array[size];
+            int sum = 0;
+            int count = 0;
+            initializeArray(array, 0, size);
+            applyCriteria(array, 0, size, count, sum);
+
+            for (int i = 0; i < size; i++) {
+                if (array[i] > 0 && array[i] % 3 != 0) {
+                    Assert::AreEqual(array[i], 0);
+                }
+            }
+        }
+    };
+}
