24721f499b6b4defc75d04e86a199a9d46bb33af
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-19 19:18:12 +0200

    Add files via upload

---
+++ Pract_6_1IM/Pract_6_1IM/Pract_6_1IM.cpp
@@ -0,0 +1,45 @@
+﻿#include <time.h>
+#include <iostream>
+using namespace std;
+
+int* applyCriteria(int* array, int size, int& sum, int& count) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] > 0 && array[i] % 3 != 0) {
+            count++;
+            sum += array[i];
+        }
+        else {
+            array[i] = 0;
+        }
+    }
+    return array;
+}
+
+int main() {
+    const int arraySize = 22;
+    int sum = 0;
+    int count = 0;
+    int array[arraySize];
+
+    srand((unsigned)time(NULL));
+
+   
+    for (int i = 0; i < arraySize; i++) {
+        array[i] = -10 + rand() % (35 + 10 + 1);
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+
+  
+    applyCriteria(array, arraySize, sum, count);
+
+    cout << "sum: " << sum << "\n";
+    cout << "count: " << count << "\n";
+
+  
+    for (int i = 0; i < arraySize; i++) {
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
---
+++ Pract_6_1IM/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_6_1IM/Pract_6_1IM.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestApplyCriteria)
+		{
+			const int arraySize = 10;
+			int array[arraySize];
+			int sum = 0;
+			int count = 0;
+
+			int* result = applyCriteria(array, arraySize, sum, count);
+
+	
+			for (int i = 0; i < arraySize; i++) {
+				if (array[i] > 0 && array[i] % 3 != 0) {
+					Assert::AreEqual(result[i], 0);
+				}
+			}
+
+	
+			Assert::AreEqual(sum, 0);
+			Assert::AreEqual(count, 0);
+		}
+
+	
+	};
+}
