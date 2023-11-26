f6161e80b41197c471092a182c23e2682231cf2d
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-26 22:47:06 +0200

    Add files via upload

---
+++ Pract_6_4rec/Pract_6_4rec/Pract_6_4rec.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int findMaxIndexRecursive(const int* array, int size, int currentIndex = 0, int maxIndex = 0) {
+    if (currentIndex == size) {
+        return maxIndex;
+    }
+
+    if (array[currentIndex] > array[maxIndex]) {
+        maxIndex = currentIndex;
+    }
+
+    return findMaxIndexRecursive(array, size, currentIndex + 1, maxIndex);
+}
+
+int findProductBetweenZerosRecursive(const int* array, int size, int currentIndex = 0, int firstZeroIndex = -1, int secondZeroIndex = -1) {
+    if (currentIndex == size) {
+        if (firstZeroIndex != -1 && secondZeroIndex != -1 && firstZeroIndex < secondZeroIndex) {
+            int product = 1;
+            for (int i = firstZeroIndex + 1; i < secondZeroIndex; ++i) {
+                product *= array[i];
+            }
+            return product;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    if (array[currentIndex] == 0) {
+        if (firstZeroIndex == -1) {
+            firstZeroIndex = currentIndex;
+        }
+        else {
+            secondZeroIndex = currentIndex;
+        }
+    }
+
+    return findProductBetweenZerosRecursive(array, size, currentIndex + 1, firstZeroIndex, secondZeroIndex);
+}
+
+void rearrangeArrayRecursive(int* array, int size, int currentIndex = 0) {
+    if (currentIndex < size / 2) {
+        if (currentIndex % 2 == 0) {
+            int temp = array[currentIndex];
+            array[currentIndex] = array[size / 2 + currentIndex];
+            array[size / 2 + currentIndex] = temp;
+        }
+
+        rearrangeArrayRecursive(array, size, currentIndex + 1);
+    }
+}
+
+int main() {
+    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n;
+    std::cout << "Введіть розмір масиву: " << std::endl;
+    std::cin >> n;
+
+    int* array = new int[n];
+
+    std::cout << "Введіть елементи масиву:\n" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> array[i];
+    }
+
+    int maxIndex = findMaxIndexRecursive(array, n);
+    std::cout << "Номер максимального елементу: " << maxIndex << std::endl;
+
+    int productBetweenZeros = findProductBetweenZerosRecursive(array, n);
+    std::cout << "Добуток елементів між першим і другим нульовими елементами: " << productBetweenZeros << std::endl;
+
+    rearrangeArrayRecursive(array, n);
+
+    std::cout << "Відсортований масив:\n" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] array;
+
+    return 0;
+}
---
+++ Pract_6_4rec/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_6_4rec/Pract_6_4rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestFindMaxIndexRecursive)
+		{
+			const int size = 5;
+			int array[size] = { 3, 8, 1, 5, 6 };
+
+			int maxIndex = findMaxIndexRecursive(array, size);
+
+			Assert::AreEqual(1, maxIndex);
+		}
+
+		TEST_METHOD(TestFindProductBetweenZerosRecursive)
+		{
+			const int size = 8;
+			int array[size] = { 2, 4, 0, 7, 1, 0, 3, 6 };
+
+			int product = findProductBetweenZerosRecursive(array, size);
+
+			Assert::AreEqual(7, product);
+		}
+
+		// Додайте інші тести за потреби
+
+	};
+}
