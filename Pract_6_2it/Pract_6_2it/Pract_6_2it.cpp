a0cd71bfa524cb45cec919c5e8eadf3921f35040
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-26 18:40:18 +0200

    Add files via upload

---
+++ Pract_6_2it/Pract_6_2it/Pract_6_2it.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace  std;
+int findIndexOfMinOdd(const vector<int>& array) {
+    int minOddIndex = -1;
+    int minOddValue = INT_MAX;
+
+    for (int i = 0; i < array.size(); ++i) {
+        if (array[i] % 2 != 0 && array[i] < minOddValue) {
+            minOddValue = array[i];
+            minOddIndex = i;
+        }
+    }
+
+    return minOddIndex;
+}
+
+int main() {
+
+    vector<int> myVector = { 2, 7, 1, 9, 4, 6, 5 };
+
+    int resultIndex = findIndexOfMinOdd(myVector);
+
+    if (resultIndex != -1) {
+        cout << "index naimenshogo elementa " << resultIndex << endl;
+    }
+    else {
+        cout << "U vektori nemaye neparnux chusel" << endl;
+    }
+
+    return 0;
+}
---
+++ Pract_6_2it/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_6_2it/Pract_6_2it.cpp" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace YourProjectTests
+{
+    TEST_CLASS(YourTests)
+    {
+    public:
+
+        TEST_METHOD(TestFindIndexOfMinOdd)
+        {
+            
+            vector<int> testVector1 = { 2, 7, 1, 9, 4, 6, 5 };
+            vector<int> testVector2 = { 2, 4, 6, 8, 10 };
+
+           
+            int resultIndex1 = findIndexOfMinOdd(testVector1);
+            int resultIndex2 = findIndexOfMinOdd(testVector2);
+
+           
+            Assert::AreEqual(2, resultIndex1); 
+            Assert::AreEqual(-1, resultIndex2); 
+        }
+    };
+}
