7a26346e47b2e1e33e20224d0d9ec45a972c335b
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-26 18:43:36 +0200

    Add files via upload

---
+++ Pract_6_3rec/Pract_6_3rec/Pract_6_3rec.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void sortDescendingRecursive(int array[], int start, int end) {
+    if (start < end) {
+        int maxIndex = start;
+        for (int i = start + 1; i <= end; ++i) {
+            if (array[i] > array[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        swap(array[start], array[maxIndex]);
+        sortDescendingRecursive(array, start + 1, end);
+    }
+}
+
+int main() {
+    
+    const int arraySize = 5;
+    int myArray[arraySize] = { 3, 1, 4, 2, 5 };
+
+    cout << "Masyv: ";
+    for (int i = 0; i < arraySize; ++i) {
+        std::cout << myArray[i] << " ";
+    }
+
+    sortDescendingRecursive(myArray, 0, arraySize - 1);
+
+    cout << "\nVidcortovanyi masyv: ";
+    for (int i = 0; i < arraySize; ++i) {
+        cout << myArray[i] << " ";
+    }
+
+    return 0;
+}
---
+++ Pract_6_3rec/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pract_6_3rec/pract_6_3rec.cpp" 
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace pract_6_3recTests
+{
+    TEST_CLASS(RecursiveTests)
+    {
+    public:
+
+        TEST_METHOD(TestSortDescendingRecursive)
+        {
+
+            const int arraySize = 5;
+            int testArray1[arraySize] = { 3, 1, 4, 2, 5 };
+            int testArray2[arraySize] = { 5, 4, 3, 2, 1 };
+
+            sortDescendingRecursive(testArray1, 0, arraySize - 1);
+            sortDescendingRecursive(testArray2, 0, arraySize - 1);
+
+    
+            for (int i = 0; i < arraySize - 1; ++i) {
+                Assert::IsTrue(testArray1[i] >= testArray1[i + 1]); 
+                Assert::IsTrue(testArray2[i] >= testArray2[i + 1]); 
+            }
+        }
+    };
+}
