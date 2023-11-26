d46645333e551df9f39e09cbd2f8e0dafb695d38
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-26 18:41:23 +0200

    Add files via upload

---
+++ Pract_6_2rec/Pract_6_2rec/Pract_6_2rec.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int findIndexOfMinOddRecursive(const std::vector<int>& array, int currentIndex, int minOddIndex) {
+    if (currentIndex < array.size()) {
+        if (array[currentIndex] % 2 != 0 && (minOddIndex == -1 || array[currentIndex] < array[minOddIndex])) {
+            return findIndexOfMinOddRecursive(array, currentIndex + 1, currentIndex);
+        }
+        else {
+            return findIndexOfMinOddRecursive(array, currentIndex + 1, minOddIndex);
+        }
+    }
+    else {
+        return minOddIndex;
+    }
+}
+
+int main() {
+
+    vector<int> myVector = { 2, 7, 1, 9, 4, 6, 5 };
+
+    int resultIndex = findIndexOfMinOddRecursive(myVector, 0, -1);
+
+    if (resultIndex != -1) {
+       cout << "Index naimeshogo: " << resultIndex << endl;
+    }
+    else {
+        cout << "U vektori nema nep znach" << endl;
+    }
+
+    return 0;
+}
---
+++ Pract_6_2rec/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_6_2rec/Pract_6_2rec.cpp"  
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Pract_6_2recTests
+{
+    TEST_CLASS(RecursiveTests)
+    {
+    public:
+
+        TEST_METHOD(TestFindIndexOfMinOddRecursive)
+        {
+         
+        vector<int> testVector1 = { 2, 7, 1, 9, 4, 6, 5 };
+        vector<int> testVector2 = { 2, 4, 6, 8, 10 };
+
+     
+            int resultIndex1 = findIndexOfMinOddRecursive(testVector1, 0, -1);
+            int resultIndex2 = findIndexOfMinOddRecursive(testVector2, 0, -1);
+
+   
+            Assert::AreEqual(2, resultIndex1);
+            Assert::AreEqual(-1, resultIndex2); 
+        }
+    };
+}
