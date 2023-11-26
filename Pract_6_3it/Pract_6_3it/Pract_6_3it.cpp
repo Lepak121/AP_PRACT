be34c25954cafe984ad9fe4c442053dbb03c964f
Lepak121 <145373846+Lepak121@users.noreply.github.com>
2023-11-26 18:42:25 +0200

    Add files via upload

---
+++ Pract_6_3it/Pract_6_3it/Pract_6_3it.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+void sortDescending(int array[], int arraySize) {
+    for (int i = 0; i < arraySize - 1; ++i) {
+        for (int j = i + 1; j < arraySize; ++j) {
+            if (array[i] < array[j]) {
+               
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    
+    const int arraySize = 5;
+    int myArray[arraySize] = { 3, 1, 4, 2, 5 };
+
+    cout << "Masyv: ";
+    for (int i = 0; i < arraySize; ++i) {
+        cout << myArray[i] << " ";
+    }
+
+    sortDescending(myArray, arraySize);
+
+    cout << "\nvidsortovaniy masyv ";
+    for (int i = 0; i < arraySize; ++i) {
+        cout << myArray[i] << " ";
+    }
+
+    return 0;
+}
---
+++ Pract_6_3it/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pract_6_3it/Pract_6_3it.cpp"  
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Pract_6_3itTests
+{
+    TEST_CLASS(IterativeTests)
+    {
+    public:
+
+        TEST_METHOD(TestSortDescending)
+        {
+         
+            const int arraySize = 5;
+            int testArray1[arraySize] = { 3, 1, 4, 2, 5 };
+            int testArray2[arraySize] = { 5, 4, 3, 2, 1 };
+
+   
+            sortDescending(testArray1, arraySize);
+            sortDescending(testArray2, arraySize);
+
+            for (int i = 0; i < arraySize - 1; ++i) {
+                Assert::IsTrue(testArray1[i] >= testArray1[i + 1]); 
+                Assert::IsTrue(testArray2[i] >= testArray2[i + 1]); 
+            }
+        }
+    };
+}
